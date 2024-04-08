086174a8e855452245408b3442ae37333528ff93
SimoneOlmi <166011442+SimoneOlmi@users.noreply.github.com>
2024-04-08 14:59:05 +0200

    Logica ristorante

---
+++ esercizio_ristorante.py
@@ -0,0 +1,10 @@
+"""
+Creo una classe Ristorante. In questa classe ci metto un metodo __init__ con parametri: self, nome e tipo di cucina. ci metto
+un booleano descrizione=False che mi flagga se la descrizione del ristorante è attiva o no. Ci metto poi un attributo di classe inizializzato 
+False per flaggare il ristorante come chiuso. Creo poi un dizionario vuoto {} in cui andrò poi a mettere prezzo e piatto.
+Creo un metodo apertura ristorante che mi cambia il flag del ristorante in True, un metodo chiusura che me lo setta a False, un metodo setta_descrizione
+che mi setta la descrizione True, un metodo Togli_descrizione che mi mette la descrizione false. Poi metto un metodo stato_apertura che mi dice che il ristorante è
+aperto se il parametro è True, il contrario se False. Aggiungi al menù sarà un metodo che prende come parametro il nome ed il prezzo del piatto e me lo inserisce nel dizionario
+definito prima. La stessa cosa con il metodo togli dal menù, ma andrò a toglierlo. Infine, stampa menù stamperà tutte le chiavi del nome del piatto col rispettivo valore di prezzo. 
+
+"""
