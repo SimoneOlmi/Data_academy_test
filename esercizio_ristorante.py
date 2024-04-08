adeaaa27b7fe1071a1bb519e54d0139becc36721
SimoneOlmi <166011442+SimoneOlmi@users.noreply.github.com>
2024-04-08 15:27:46 +0200

    Aggiunta classe e alcuni test

--- esercizio_ristorante.py
+++ esercizio_ristorante.py
@@ -8,3 +8,83 @@ aperto se il parametro è True, il contrario se False. Aggiungi al menù sarà u
 definito prima. La stessa cosa con il metodo togli dal menù, ma andrò a toglierlo. Infine, stampa menù stamperà tutte le chiavi del nome del piatto col rispettivo valore di prezzo. 
 
 """
+
+class Ristorante:
+
+    apertura = False
+    descrizione = False
+
+    #metodo costruttore con due parametri e un dizionario vuoto
+    def __init__(self, nome, tipo_cucina):
+        self.nome = nome
+        self.tipo_cucina = tipo_cucina
+
+        self.menù = {}
+
+    #metodo apertura ristorante che mi flagga True quando chiamato
+    def apertura_ristorante(self):
+        self.apertura = True
+
+    #metodo chiusura ristorante che mi flagga false quando chiamato
+    def chiusura_ristorante(self):
+        self.apertura = False
+    
+    #metodo metti descrizione che mi flagga true quando chiamato
+    def metti_descrizione(self):
+        self.descrizione = True
+    
+    #metodo togli descrizione che mi flagga false quando chiamato
+    def togli_descrizione(self):
+        self.descrizione = False
+
+    #iniziamo con lo stato dell'apertura del ristorante
+    def stato_apertura(self):
+        if self.apertura == True:
+            print("Il ristorante è aperto! Benvenuto\n")
+        elif self.apertura == False:
+            print("Il ristorante è chiuso. Ci dispiace\n")
+
+    #stampo la descrizione del ristorante, se il flag è true
+    def descrizione_ristorante(self):
+        if self.descrizione == True:
+            print("Il Ristorante", self.nome,"è un'accogliente osteria situata nel cuore di una pittoresca piazza nel centro storico.\nOffriamo un'autentica esperienza culinaria" , self.tipo_cucina,"celebrando le tradizioni e i sapori della cucina casalinga", self.tipo_cucina,".\nCon ingredienti freschi e di alta qualità provenienti direttamente dai mercati locali, i nostri piatti sono preparati con cura e passione.\nDal classico risotto alla milanese alle fragranti pizze appena sfornate, ogni piatto è un omaggio alla ricca cultura gastronomica",self.tipo_cucina,".\nSiamo orgogliosi di offrire un ambiente familiare e accogliente, dove gli ospiti possono gustare piatti deliziosi e creare ricordi indimenticabili con amici e familiari.\nBenvenuti a", self.nome,", dove il buon cibo e la convivialità si incontrano!\n")
+        else:
+            print("Descrizione non disponibile al momento. Siate pazienti\n")    
+
+    #metodo per aggiungere piatti al menù
+    def aggiungi_al_menù(self):
+        nome_piatto = input("Inserisci il nome del piatto che vorresti aggiungere: ")
+        prezzo = int(input("Inserisci il nome del prezzo del piatto aggiunto: "))
+        self.menù[nome_piatto] = prezzo
+    
+    #metodo per rimuovere piatto dal menù
+    def togli_dal_menù(self):
+        nome_piatto = input("Inserisci il nome del piatto da rimuovere dal menù: ")
+        if nome_piatto not in self.menù:
+            print("Il piatto selezionato non è presente nel menù\n")
+        else:
+            del self.menù[nome_piatto]
+    
+    #metodo per stampare il menù
+    def stampa_menù(self):
+        print(self.menù)
+
+
+
+test = Ristorante("Trattoria di Simi", "Italiana")
+
+test.metti_descrizione()
+
+test.descrizione_ristorante()
+
+test.togli_descrizione()
+
+test.stato_apertura()
+
+test.apertura_ristorante()
+
+test.aggiungi_al_menù()
+
+test.stampa_menù()
+
+test.stato_apertura()
