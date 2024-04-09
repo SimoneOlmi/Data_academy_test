a13de1a80852d7e7f2fde8acc7dfbc6f9a4d3683
SimoneOlmi <166011442+SimoneOlmi@users.noreply.github.com>
2024-04-09 15:57:03 +0200

    logica_iniziale

---
+++ esercizio_libreria.py
@@ -0,0 +1,6 @@
+# Creo una classe libreria. creo al suo interno un attributo catalogo, un dizionario con chiave isbn e valori autore e titolo (lista dentro dizionario). aggiungo il metodo 
+# aggiungi libro, che andrà ad associarmi un nuovo isbn unico (checko con un if se è già presente un isbn uguale all'interno) legato a valori titolo e autore.
+# rendo libro padre di libreria? Da pensarci
+# rimuovi libro è come aggiungi libro, ma cerco per chiave del dizionario 
+# cerca per titolo creo un ciclo for per la lunghezza di tutti i miei libri e cerco se il titolo è == al mio input. questo metodo ha un parametro titolo_cercato
+# creo una classe libro con init titolo, autore, isbn. Fare un metodo print che stampa i tre parametri. 
