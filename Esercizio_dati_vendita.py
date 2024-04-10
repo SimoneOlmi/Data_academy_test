bd30cc04e20087698640ed687d340b9663e1101f
SimoneOlmi <166011442+SimoneOlmi@users.noreply.github.com>
2024-04-10 10:44:49 +0200

    logica_iniziale

---
+++ Esercizio_dati_vendita.py
@@ -0,0 +1,6 @@
+# Creare una classe Vendite con all'interno  di questa classe ci metto una lista vuota che rappresenta l'importo totale per giorno. farò un metodo calcolo totale
+# che sommerà ogni elemento della lista vendite per giorno. Farò un metodo calcola media, che calcolerà la media aritmetica di ogni elemento nella lista vendite totale.
+# Farò un metodo che calcola il giorno di vendite massimo, restituendo il giorno e il valore in cui si è venduto di più.
+# Farò poi una classe figlio Giorno. come attributi la classe giorno prenderà il giorno della vendita e l'importo del giorno. 
+# Mi serve un metodo aggiungi vendite, che devo ancora decidere se mettere nella classe padre o nella classe figlio. Andrò comunque a richiamare questo metodo con un oggetto
+# della classe figlio, aggiungendo il valore di vendita e il giorno.
